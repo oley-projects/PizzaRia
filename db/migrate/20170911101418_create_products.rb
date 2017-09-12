3684a39acdfa648aef272caa4e3100051ba8cca3
Oley-Projects <oley.for.git@gmail.com>
2017-09-12 16:28:11 +0300

    add pizza's description

--- db/migrate/20170911101418_create_products.rb
+++ db/migrate/20170911101418_create_products.rb
@@ -1,8 +1,8 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
   	create_table :products do |t|
-  	  t.string 	:title
-  	  t.text 	:description
+  	  t.string  :title
+  	  t.text 	  :description
   	  t.decimal :price
   	  t.decimal :size
   	  t.boolean :is_spicy
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911101418) do
+ActiveRecord::Schema.define(version: 20170912130547) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
